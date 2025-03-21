a94fb1fbb758e83681c0640ce38d391feced30b8
NotAFatalist <eldardeineckin@yandex.ru>
2025-03-21 23:12:21 +0300

    add teg <t>

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 from flask import Flask, render_template, request
 import markdown2
+import re
 
 app = Flask(__name__)
+
+def convert_tasks(text):
+    def replace_task(match):
+        task_text = match.group(1)
+        return f'<input type="checkbox"> {task_text}<br>'
+    
+    return re.sub(r'<t>(.*?)</t>', replace_task, text)
 
 @app.route('/', methods=['GET', 'POST'])
 def editor():
@@ -9,6 +17,7 @@ def editor():
     text = ""
     if request.method == 'POST':
         text = request.form['text']
+        text = convert_tasks(text)
         html = markdown2.markdown(text, extras=["fenced-code-blocks", "tables", "strike"])
     return render_template('editor.html', text=text, html=html)
 
